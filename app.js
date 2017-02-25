183fb31bf64d38404abce51846810edf71e90658
DESKTOP-90SHPSH\tf2bi <tf2bix@gmail.com>
2017-02-25 17:34:07 +0800

    Update 6.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var score = 0;
 var life = 3;
 
 function preload() {
-  game.load.image('sky', 'assets/starfield2.png');
+  game.load.image('sky', 'assets/sky2.png');
   game.load.image('ground', 'assets/platform.png');
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
