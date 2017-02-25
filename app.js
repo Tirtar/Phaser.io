59cd98fd3cb0295010da5eea664bd0471d3193af
DESKTOP-90SHPSH\tf2bi <tf2bix@gmail.com>
2017-02-25 17:32:00 +0800

    Update 5.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var score = 0;
 var life = 3;
 
 function preload() {
-  game.load.image('sky', 'assets/sky.png');
+  game.load.image('sky', 'assets/starfield2.png');
   game.load.image('ground', 'assets/platform.png');
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
@@ -207,6 +207,6 @@ function loseLifeLeft (player, enemy) {
   	enemy1.kill();
   	enemy2.kill();
   	enemy3.kill();
-  	
+
   }
 }
